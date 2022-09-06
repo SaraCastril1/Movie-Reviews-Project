eba44a1f6f059d1c5258640013061bbc5384f486
SaraCastril1 <84990901+SaraCastril1@users.noreply.github.com>
2022-09-06 14:06:28 -0500

    Formulario de búsqueda

--- movie/views.py
+++ movie/views.py
@@ -10,11 +10,12 @@ def home(request):
    #return render(request, 'home.html')
    #return render(request, 'home.html', {'name': 'Sara Castrillón'})
    searchTerm =request.GET.get('searchMovie')
-   if searchTerm:
-      movies = Movie.objects.filter(title__icontains=searchTerm)
-   else:
-      movies = Movie.objects.all()
-   return render(request, 'home.html', {'searchTerm':searchTerm, 'movies': movies})
+   return render(request, 'home.html', {'searchTerm':searchTerm})
+   #if searchTerm:
+   #   movies = Movie.objects.filter(title__icontains=searchTerm)
+   #else:
+   #   movies = Movie.objects.all()
+   #return render(request, 'home.html', {'searchTerm':searchTerm, 'movies': movies})
 
 def about(request):
    return HttpResponse('<h1>Welcome to About Page</h1>')
